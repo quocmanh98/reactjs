b6fbaa180efd7027330f47443bc7314ec3021bf6
Quoc Manh <manhquoc1998s@gmail.com>
2023-08-20 14:56:52 +0700

    React Component - Làm Quen với Cách Viết Component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MyComponent extends React.Component
+{
+    render()
+    {
+        return (
+            <div>123</div>
+        )
+    }
+}
+
+export default MyComponent
